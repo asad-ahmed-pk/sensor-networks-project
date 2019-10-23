57187f70b72bd8dcd968668e62d4e65655a1f675
Asad Ahmed <asad.ahmed.ae@icloud.com>
2019-10-23 10:56:50 +0200

    Added fake serial for testing

--- temp_db_logger/read_serial.py
+++ temp_db_logger/read_serial.py
@@ -16,3 +16,17 @@ class SerialReader:
 
     def close(self):
         self.serial.close()
+
+
+class FakeSerialReader:
+    """ Reads from the serial port """
+    def __init__(self, port):
+        print("Fake Serial port initialised on", port)
+        self.value = 0.1
+
+    def read_next_value(self):
+        self.value += 0.1
+        return self.value
+
+    def close(self):
+        print("Serial closed")
