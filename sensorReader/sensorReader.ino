480bf1b098cc358d098c9731b15e4bc57018998c
Asad Ahmed <asad.ahmed.ae@icloud.com>
2019-10-23 12:17:00 +0200

    Add files via upload

---
+++ sensorReader/sensorReader.ino
@@ -0,0 +1,21 @@
+#include<SoftwareSerial.h>
+
+int TMP36 = A0;
+int temperature = 0;
+int wait_ms = 500;
+short xBeeTx = 12;
+short xBeeRx = 11;
+
+
+SoftwareSerial xBeeSerial (xBeeRx, xBeeTx);
+void setup() {
+  Serial.begin(9600);
+  xBeeSerial.begin(9600);
+}
+
+void loop() {
+  temperature = map(analogRead(TMP36), 0 ,410, -50, 150);
+  xBeeSerial.println(temperature);
+  Serial.println(temperature);
+  delay(wait_ms);
+}
---
+++ sensorReciever/sensorReciever.ino
@@ -0,0 +1,47 @@
+#include<SoftwareSerial.h>
+
+short xBeeTx = 12;
+short xBeeRx = 11;
+String temperature;
+
+char data[3]; 
+
+SoftwareSerial xBeeSerial(xBeeRx, xBeeTx);
+
+void setup() {
+  Serial.begin(9600);
+  xBeeSerial.begin(9600);
+}
+
+void loop() 
+{
+  if(xBeeSerial.available() > 0)
+  {
+    //temperature = xBeeSerial.readStringUntil('\n');
+    //int tempint = temperature.toInt();
+    int val = 0;
+    int i = 0;
+    while (val != 13)
+    {
+      val = xBeeSerial.read();
+      Serial.println(val);
+      if (val != 13) {
+        data[i] = val;
+        i++;
+      }
+      else {
+        data[2] = '\0';
+      }
+    }
+
+    // string built convert from ASCII
+    for (int i = 0; i < 3; i++) {
+      //Serial.println(data[i]);
+    }
+
+    // discard useless newline
+    xBeeSerial.read();
+    
+    Serial.println();
+  }
+}
